b979dbb34b82ea27f6d7086f87a825eb8f8a2bdb
SweetSoundsMusicSchool <122542216+SweetSoundsMusicSchool@users.noreply.github.com>
2023-02-05 10:47:19 -0500

    Add files via upload

---
+++ SweetSoundsMusic/SweetSoundsMusicSchool/src/main/java/SweetSoundsMusic/SweetSoundsMusicSchoolApplication.java
@@ -0,0 +1,13 @@
+package SweetSoundsMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetSoundsMusicSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetSoundsMusicSchoolApplication.class, args);
+	}
+
+}
---
+++ SweetSoundsMusic/SweetSoundsMusicSchool/src/test/java/SweetSoundsMusic/SweetSoundsMusicSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package SweetSoundsMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetSoundsMusicSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
